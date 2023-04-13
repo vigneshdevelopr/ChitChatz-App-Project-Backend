5d4e613423b3c69eb5bda8eea482b95a475ca325
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-04-13 22:26:23 +0530

    finalcommit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(PORT, () => {
 const io = new Server(server, {
   pingTimeout:60000,
     cors:{
-        origin: "*",
+        origin: "http://localhost:3000",
         credentials: true
 
     },
